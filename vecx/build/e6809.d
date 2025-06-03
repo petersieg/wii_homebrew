8c536107b0a6c567503e33f4f1ec7d46d208194e
Peter Sieg <16632721+petersieg@users.noreply.github.com>
2025-06-03 18:55:01 +0200

    Delete vecx directory

--- vecx/build/e6809.d
+++
@@ -1,3 +0,0 @@
-e6809.o: /Users/ich/wii/apps/vecx/source/e6809.c \
- /Users/ich/wii/apps/vecx/source/e6809.h
-/Users/ich/wii/apps/vecx/source/e6809.h:
--- vecx/build/vecx.d
+++
@@ -1,9 +0,0 @@
-vecx.o: /Users/ich/wii/apps/vecx/source/vecx.c \
- /Users/ich/wii/apps/vecx/source/e6809.h \
- /Users/ich/wii/apps/vecx/source/vecx.h \
- /Users/ich/wii/apps/vecx/source/osint.h \
- /Users/ich/wii/apps/vecx/source/e8910.h
-/Users/ich/wii/apps/vecx/source/e6809.h:
-/Users/ich/wii/apps/vecx/source/vecx.h:
-/Users/ich/wii/apps/vecx/source/osint.h:
-/Users/ich/wii/apps/vecx/source/e8910.h:
